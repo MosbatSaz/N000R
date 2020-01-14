b291fe641ed302799596023be4226b462aea1559
7Skiez <51506653+7Skiez@users.noreply.github.com>
2020-01-14 18:23:25 +0330

    Revert "Revert "-""
    This reverts commit 09476c1f50c105481c2f691d4c6580be9dcfb06b.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
 Vue.use(Vuetify);
 
